87f623cfa058fbf922fd1494a8ca063450e9ccd4
aw811502 <77753326+aw811502@users.noreply.github.com>
2022-09-22 23:30:37 -0400

    Implemented help text

--- cli.js
+++ cli.js
@@ -1,1 +1,13 @@
 #!/usr/bin/env node
+
+const fs = require('fs');
+const parseArgs = require('minimist');
+
+const args = parseArgs(process.argv.slice(2));
+
+if (args.h) {
+  const buffer = fs.readFileSync("help.txt");
+  const helpText = buffer.toString();
+  console.log(helpText);
+  process.exit(0);
+}
